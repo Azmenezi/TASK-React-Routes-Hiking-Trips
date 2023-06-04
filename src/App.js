f3a8d47a0a79e933e5e0721678afc974061298f6
Azmenezi <azmenezi@gmail.com>
2023-06-04 18:32:21 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import TripsList from './components/TripsList';
-import TripDetail from './components/TripDetail';
-
+import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import TripsList from "./components/TripsList";
+import TripDetail from "./components/TripDetail";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div id="page-top">
       <Nav />
-      <Home />
-      <TripsList />
-      <TripDetail />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/trips-list" element={<TripsList />} />
+        <Route path="/trips-detail/:tripId" element={<TripDetail />} />
+      </Routes>
     </div>
   );
 }
--- src/components/TripDetail.js
+++ src/components/TripDetail.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import tripsData from '../tripsData';
-
+import React from "react";
+import tripsData from "../tripsData";
+import { useParams, Navigate } from "react-router-dom";
 function TripDetail() {
-  const trip = tripsData[0];
+  // const trip = tripsData;
+  const { tripId } = useParams();
+  const trip = tripsData.find((element) => element.name.replace(/\s+/g, "-").toLowerCase() == tripId);
+  if (!trip) {
+    return <Navigate to="/" replace={true} />;
+  }
   return (
     <div className="modal-dialog modal-xl">
       <div className="modal-content">
